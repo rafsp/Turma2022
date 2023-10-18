0e141c285461bcd544b799c354fc70fc539ff3db
RENAN MAGALHAES <renan.magalhaes@etec.sp.gov.br>
2023-10-17 21:52:58 -0300

    Alteração feita as 21:49

--- ProjetoCRUD/Tela3.Designer.cs
+++ ProjetoCRUD/Tela3.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -41,11 +42,22 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "2 módulo de DS";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label2.Location = new System.Drawing.Point(211, 269);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(362, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Alteração feita com sucesso!";
+            // 
             // Tela3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Tela3";
             this.Text = "Tela3";
@@ -57,5 +69,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
