29a045cc8ed9a2fe67adca60a0d83ee6acf2cddc
RAFAEL PEREIRA <rafael.pereira236@etec.sp.gov.br>
2023-10-24 20:34:17 -0300

    projeto completo

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvAluno = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCodigoAluno = new System.Windows.Forms.TextBox();
+            this.txtNomeAluno = new System.Windows.Forms.TextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.pnlAlterar = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAluno)).BeginInit();
+            this.pnlAlterar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvAluno
+            // 
+            this.dgvAluno.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAluno.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvAluno.Location = new System.Drawing.Point(31, 32);
+            this.dgvAluno.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvAluno.Name = "dgvAluno";
+            this.dgvAluno.RowHeadersWidth = 51;
+            this.dgvAluno.RowTemplate.Height = 24;
+            this.dgvAluno.Size = new System.Drawing.Size(407, 147);
+            this.dgvAluno.TabIndex = 0;
+            this.dgvAluno.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvAluno_CellMouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Código do Aluno";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Endereço do Aluno";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nome do Aluno";
+            // 
+            // txtCodigoAluno
+            // 
+            this.txtCodigoAluno.Enabled = false;
+            this.txtCodigoAluno.Location = new System.Drawing.Point(128, 9);
+            this.txtCodigoAluno.Name = "txtCodigoAluno";
+            this.txtCodigoAluno.Size = new System.Drawing.Size(129, 20);
+            this.txtCodigoAluno.TabIndex = 4;
+            // 
+            // txtNomeAluno
+            // 
+            this.txtNomeAluno.Location = new System.Drawing.Point(128, 35);
+            this.txtNomeAluno.Name = "txtNomeAluno";
+            this.txtNomeAluno.Size = new System.Drawing.Size(129, 20);
+            this.txtNomeAluno.TabIndex = 4;
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(128, 61);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(129, 20);
+            this.txtEndereco.TabIndex = 4;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(128, 87);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 25);
+            this.btnAlterar.TabIndex = 5;
+            this.btnAlterar.Text = "Alterar Aluno";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // pnlAlterar
+            // 
+            this.pnlAlterar.Controls.Add(this.txtEndereco);
+            this.pnlAlterar.Controls.Add(this.btnAlterar);
+            this.pnlAlterar.Controls.Add(this.label1);
+            this.pnlAlterar.Controls.Add(this.label2);
+            this.pnlAlterar.Controls.Add(this.txtNomeAluno);
+            this.pnlAlterar.Controls.Add(this.label3);
+            this.pnlAlterar.Controls.Add(this.txtCodigoAluno);
+            this.pnlAlterar.Location = new System.Drawing.Point(31, 196);
+            this.pnlAlterar.Name = "pnlAlterar";
+            this.pnlAlterar.Size = new System.Drawing.Size(294, 117);
+            this.pnlAlterar.TabIndex = 6;
+            this.pnlAlterar.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 381);
+            this.Controls.Add(this.pnlAlterar);
+            this.Controls.Add(this.dgvAluno);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAluno)).EndInit();
+            this.pnlAlterar.ResumeLayout(false);
+            this.pnlAlterar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvAluno;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCodigoAluno;
+        private System.Windows.Forms.TextBox txtNomeAluno;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Panel pnlAlterar;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CarregarDadosBanco();
+
+          }
+
+        private void CarregarDadosBanco()
+        {
+            string conexao = "server=localhost;database=aula1209;uid=root;pwd=";
+            MySqlConnection conexaoMYSQL = new MySqlConnection(conexao);
+            conexaoMYSQL.Open();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter("select * from Aluno", conexaoMYSQL);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dgvAluno.DataSource = dt;
+
+        }
+
+
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            string conexao = "server=localhost;database=aula1209;uid=root;pwd=";
+            MySqlConnection conexaoMYSQL = new MySqlConnection(conexao);
+            conexaoMYSQL.Open();
+            MySqlCommand comando = new MySqlCommand("update aluno set nome='" + txtNomeAluno.Text + "', dsEndereco='" + txtEndereco.Text + "' where idAluno=" + txtCodigoAluno.Text, conexaoMYSQL);
+            comando.ExecuteNonQuery();
+            MessageBox.Show("Dados alterados!!!");
+            txtNomeAluno.Text = "";
+            txtEndereco.Text = "";
+            txtCodigoAluno.Text = "";
+            pnlAlterar.Visible = false;
+            CarregarDadosBanco();
+        }
+
+        private void dgvAluno_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            
+            txtCodigoAluno.Text = dgvAluno.Rows[e.RowIndex].Cells[0].Value.ToString();
+            txtNomeAluno.Text = dgvAluno.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtEndereco.Text = dgvAluno.Rows[e.RowIndex].Cells[2].Value.ToString();
+            pnlAlterar.Visible = true;
+
+        }
+
+
+    }
+}
